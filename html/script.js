9ccfc838d3221223b6a0561b01cd36afcf3b64f9
mahmoudv2017 <mahmoud.elbermbaly2017@yahoo.com>
2022-12-28 21:13:01 +0200

    second

--- html/script.js
+++ html/script.js
@@ -1,5 +1,8 @@
 
 
+
+
+
 document.querySelectorAll("nav a").forEach( (li , index) => {
     li.addEventListener("click" , (e) =>{
         
@@ -7,13 +10,12 @@ document.querySelectorAll("nav a").forEach( (li , index) => {
         e.target.classList.add("active")
         console.log($(e.target).hasClass("nav-link") )
         if($(e.target).hasClass("nav-link") ){
-           // document.querySelector("nav").style.top = "-4%"
-           // document.querySelector("nav").style.bottom = "95%"
-           $("nav").addClass( "scaler")
+            $("nav").addClass( "scaler")
+           
         }else{
             $("nav").removeClass("scaler")
-            //document.querySelector("nav").style.top = "0%"
-            //document.querySelector("nav").style.bottom = "88%"
+            
+
         }
         
         
